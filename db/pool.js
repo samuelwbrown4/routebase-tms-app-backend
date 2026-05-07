554fbd7d0487e76fa454c23e52c651dcf9186d69
Samuel Brown <samuelwbrown4@gmail.com>
2026-05-07 16:55:53 -0400

    configure pool for SSL, change tracking sim job to run at longer interval

--- db/pool.js
+++ db/pool.js
@@ -5,7 +5,10 @@ const pool = new Pool({
     port: process.env.DB_PORT || 5432,
     database: process.env.DB_NAME || 'tms_app',
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD || ''
+    password: process.env.DB_PASSWORD || '',
+    ssl: {
+    rejectUnauthorized: false
+  }
 })
 
 
